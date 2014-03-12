40a144d23b6c8a46ff98404f1b04e2b7696537d1
n0c0ntr0l <jacemano@gmail.com>
2014-03-12 15:17:44 +0000

    updated weather app

--- src/weatherapp/WeatherApp.java
+++ src/weatherapp/WeatherApp.java
@@ -25,6 +25,8 @@ public class WeatherApp extends JFrame {
     private JLabel jDayOfTheWeek;
     RSSReader rss = null;
     private JLabel jWindDirectionToday;
+    private JLabel jWindDirection2;
+    private JLabel jWindDirection3;
     private JLabel jWindSpeed1;
     private JLabel jMaxTemp1;
     private JLabel jMinTemp1;
@@ -49,27 +51,56 @@ public class WeatherApp extends JFrame {
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Detailed weather");
-        this.setSize(600, 300);
+        this.setSize(320, 480);
         GridBagLayout myLayout = new GridBagLayout();
         JPanel mainFrame = new JPanel(myLayout);
         rss = new RSSReader();
         GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.CENTER;
 
-
-        c.fill = GridBagConstraints.BOTH;
-        c.gridwidth = 10;
-        c.gridheight = 10;
-
-        jDayOfTheWeek = new JLabel("lelel");
-        jDayOfTheWeek.setText(this.getDayOfTheWeek());
-        c.anchor = GridBagConstraints.FIRST_LINE_START;
+        JLabel jDay = new JLabel("Day");
         c.gridx = 0;
         c.gridy = 0;
-        c.gridwidth = 10;
-        c.gridheight = 4;
-
         c.weightx = 1;
         c.weighty = 1;
+        
+        mainFrame.add(jDay,c);
+        
+        JLabel jWindD = new JLabel("Wind\n Direction"); 
+        c.gridx = 1;
+        mainFrame.add(jWindD, c);
+        
+        JLabel jWindS = new JLabel("Wind Speed");
+        c.gridx = 2;
+        mainFrame.add(jWindS, c);
+        
+        JLabel jMaxT = new JLabel("Max Temp");
+        c.gridx = 3;
+        mainFrame.add(jMaxT,c);
+        
+        JLabel jMinT = new JLabel("Min Temp");
+        c.gridx = 4;
+        mainFrame.add(jMinT, c);
+
+        JLabel jPress = new JLabel("Pressure");
+        c.gridx = 5;
+        mainFrame.add(jPress, c);
+        
+        JLabel jHumid = new JLabel("Humididty");
+        c.gridx = 6;
+        mainFrame.add(jHumid, c);
+        
+        JLabel jVis = new JLabel("Visibility");
+        c.gridx = 7;
+        mainFrame.add(jVis, c);
+        
+        
+        jDayOfTheWeek = new JLabel("lelel");
+        jDayOfTheWeek.setText(this.getDayOfTheWeek().substring(0, 3));
+        c.gridx = 0;
+        c.gridy = 1;
+
+
 
 
         mainFrame.add(jDayOfTheWeek, c);
@@ -77,30 +108,133 @@ public class WeatherApp extends JFrame {
 
 
         jWindDirectionToday = new JLabel(rss.getFirstDay().getWindDirection());
-        c.gridx = 2;
-        c.gridy = 2;
-         c.weightx = 1;
+
+        c.gridx = 1;
+        c.weightx = 1;
         c.weighty = 1;
-        c.fill = GridBagConstraints.HORIZONTAL;
         mainFrame.add(jWindDirectionToday, c);
 
 
         jWindSpeed1 = new JLabel(Integer.toString(rss.getFirstDay().getWindSpeed()) + "mph");
-        c.gridx = 3;
-        c.gridy = 3;
-         c.weightx = 1;
+        c.gridx = 2;
+        c.weightx = 1;
         c.weighty = 1;
         mainFrame.add(jWindSpeed1, c);
 
 
         jMaxTemp1 = new JLabel(Integer.toString(rss.getFirstDay().getMaxTemp()) + "C");
-        c.gridx = 4;
-        c.gridy = 4;
-         c.weightx = 1;
+        c.gridx = 3;
+        c.weightx = 1;
         c.weighty = 1;
         mainFrame.add(jMaxTemp1, c);
 
+        jMinTemp1 = new JLabel(Integer.toString(rss.getFirstDay().getMinTemp()) + "C");
+        c.gridx = 4;
+        mainFrame.add(jMinTemp1, c);
 
+        jPressure1 = new JLabel(Integer.toString(rss.getFirstDay().getPressure()));
+        c.gridx = 5;
+        mainFrame.add(jPressure1, c);
+
+        jHumidity1 = new JLabel(Integer.toString(rss.getFirstDay().getHumidity()));
+        c.gridx = 6;
+        mainFrame.add(jHumidity1, c);
+
+        jVisibility1 = new JLabel(rss.getFirstDay().getVisibility());
+        c.gridx = 7;
+        mainFrame.add(jVisibility1, c);
+        
+        
+        
+        JLabel jNextDayOfTheWeek = new JLabel(this.getNextDayofTheWeek(1).substring(0, 3));
+        c.gridx = 0;
+        c.gridy = 2;
+
+        mainFrame.add(jNextDayOfTheWeek, c);
+
+
+
+        jWindDirection2 = new JLabel(rss.getNextDay().getWindDirection());
+
+        c.gridx = 1;
+        c.weightx = 1;
+        c.weighty = 1;
+        mainFrame.add(jWindDirection2, c);
+
+
+        jWindSpeed2 = new JLabel(Integer.toString(rss.getNextDay().getWindSpeed()) + "mph");
+        c.gridx = 2;
+        c.weightx = 1;
+        c.weighty = 1;
+        mainFrame.add(jWindSpeed2, c);
+
+
+        jMaxTemp2 = new JLabel(Integer.toString(rss.getNextDay().getMaxTemp()) + "C");
+        c.gridx = 3;
+        c.weightx = 1;
+        c.weighty = 1;
+        mainFrame.add(jMaxTemp2, c);
+
+        jMinTemp2 = new JLabel(Integer.toString(rss.getNextDay().getMinTemp()) + "C");
+        c.gridx = 4;
+        mainFrame.add(jMinTemp2, c);
+
+        jPressure2 = new JLabel(Integer.toString(rss.getNextDay().getPressure()));
+        c.gridx = 5;
+        mainFrame.add(jPressure2, c);
+
+        jHumidity2 = new JLabel(Integer.toString(rss.getNextDay().getHumidity()));
+        c.gridx = 6;
+        mainFrame.add(jHumidity2, c);
+
+        jVisibility2 = new JLabel(rss.getNextDay().getVisibility());
+        c.gridx = 7;
+        mainFrame.add(jVisibility2, c);
+        
+        JLabel jNextDayOfTheWeek1 = new JLabel(this.getNextDayofTheWeek(2).substring(0, 3));
+        c.gridx = 0;
+        c.gridy = 3;
+
+        mainFrame.add(jNextDayOfTheWeek1, c);
+
+
+
+        jWindDirection3 = new JLabel(rss.getDayAfterTomorrow().getWindDirection());
+
+        c.gridx = 1;
+        c.weightx = 1;
+        c.weighty = 1;
+        mainFrame.add(jWindDirection3, c);
+
+
+        jWindSpeed3 = new JLabel(Integer.toString(rss.getDayAfterTomorrow().getWindSpeed()) + "mph");
+        c.gridx = 2;
+        c.weightx = 1;
+        c.weighty = 1;
+        mainFrame.add(jWindSpeed3, c);
+
+
+        jMaxTemp3 = new JLabel(Integer.toString(rss.getDayAfterTomorrow().getMaxTemp()) + "C");
+        c.gridx = 3;
+        c.weightx = 1;
+        c.weighty = 1;
+        mainFrame.add(jMaxTemp3, c);
+
+        jMinTemp3 = new JLabel(Integer.toString(rss.getDayAfterTomorrow().getMinTemp()) + "C");
+        c.gridx = 4;
+        mainFrame.add(jMinTemp3, c);
+
+        jPressure3 = new JLabel(Integer.toString(rss.getDayAfterTomorrow().getPressure()));
+        c.gridx = 5;
+        mainFrame.add(jPressure3, c);
+
+        jHumidity3 = new JLabel(Integer.toString(rss.getDayAfterTomorrow().getHumidity()));
+        c.gridx = 6;
+        mainFrame.add(jHumidity3, c);
+
+        jVisibility3 = new JLabel(rss.getDayAfterTomorrow().getVisibility());
+        c.gridx = 7;
+        mainFrame.add(jVisibility3, c);
 
         this.add(mainFrame);
 
@@ -117,6 +251,15 @@ public class WeatherApp extends JFrame {
 
 
     }
+
+    public String getNextDayofTheWeek(int increment) {
+        Calendar c = Calendar.getInstance(TimeZone.getDefault());
+        c.add(Calendar.DATE, increment);
+        Date currentDate = c.getTime();
+        return new SimpleDateFormat("EEEE").format(currentDate);
+    }
+    
+    
 
     public static void main(String[] args) {
         WeatherApp myapp = new WeatherApp();
